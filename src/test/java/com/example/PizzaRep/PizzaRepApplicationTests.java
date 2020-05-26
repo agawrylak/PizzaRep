07cba56aa88d95d305490b83a82b34187626ccdd
Artur Gawrylak <cyrecok@gmail.com>
2020-05-26 02:15:02 +0200

    Initial commit

---
+++ src/test/java/com/example/PizzaRep/PizzaRepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PizzaRep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaRepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
